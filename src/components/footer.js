64a9797d8f71d9c79fd11efa0602828f76557926
Edward Hermano <ejoshcasmano@gmail.com>
2024-08-17 15:06:07 -0700

    Added GitHub and LinkedIn to footer. Minor styling update to navbar.

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,14 +1,23 @@
 import React from 'react';
-import "./footer.css"
+import './footer.css'
+
+import { GitHub, LinkedIn } from '@mui/icons-material';
 
 function Footer() {
     return (
-        <div id="footer">
-            <nav id="footer-links">
-                <a className="footer-link" href="/apps">Apps</a>
-                <a className="footer-link" href="/games">Games</a>
-                <a className="footer-link" href="/about">About</a>
+        <div id='footer' className='grid-column'>
+            <nav id='footer-links'>
+                <a className='footer-link' href='/about'>About</a>
             </nav>
+            <div className='grid-row'>
+                <span>Website made by Edward Josh Hermano</span>
+                <a className='footer-link' href='https://github.com/EwJosh/ewjosh.github.io'>
+                    <GitHub />
+                </a>
+                <a className='footer-link' href='https://www.linkedin.com/in/edward-josh-hermano/'>
+                    <LinkedIn />
+                </a>
+            </div>
         </div>
     );
 }
